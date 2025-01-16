fa693cdeee193354a28b3f1464ff8bb987336ea8
Michael Harding <m.harding@lancaster.ac.uk>
2025-01-16 17:03:45 +0000

    additional pydantic classes

--- src/cr8tor/schema.py
+++ src/cr8tor/schema.py
@@ -1,7 +1,60 @@
 from enum import StrEnum
 from typing import List
-
 from pydantic import BaseModel, Field
+from datetime import datetime
+from typing import Optional
+
+#
+# Application managed data classes for entities not defined as classes in rocrate module 
+#
+
+class Organization(BaseModel):
+    name: str
+
+class SoftwareApplication(BaseModel):
+    name: str
+    provider: Organization
+
+class Agent(BaseModel):
+    name: str
+
+class Instrument(BaseModel):
+    name: str
+
+class ActionStatusType(StrEnum):
+    active_action_status: str = "ActiveActionStatus"
+    completed_action_status: str = "CompletedActionStatus"
+    failed_action_status: str = "FailedActionStatus"
+    potential_action_status: str = "PotentialActionStatus"
+
+class Action(BaseModel):
+    name: str
+    end_time: datetime
+    start_time: datetime
+    action_status: ActionStatusType
+    agent: Agent
+    error: Optional[str] = None
+    instrument: Optional[Instrument] = None
+    
+class CreateAction(Action):
+    result: List[str]
+
+class AssessAction(Action):
+    additional_type: str    
+
+#
+# Claases to model user-defined properties specified in resources TOML
+#
+
+#
+# Data access
+#
+
+class DataContract(BaseModel):
+    source: str
+    url: str
+    service_principal_key: str # key vault key holding auth info
+    secret_provider: str # i.e. Key Vault
 
 
 class CrateMeta(StrEnum):
@@ -68,7 +121,6 @@ class DatasetMetadata(BaseModel):
     catalog: str
     table_schema: str
     tables: List[TableMetadata]
-
 
 class BagitInfo(BaseModel):
     source_organization: str = Field(alias="Source-Organization")
