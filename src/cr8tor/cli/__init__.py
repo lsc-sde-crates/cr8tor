49c34d86c08a10d00d86017d8ef9ff101d4770c7
Piotr Mucha <pzm@data-ai.uk>
2025-03-19 15:57:04 +0000

    Add disclosure command to CLI

--- src/cr8tor/cli/__init__.py
+++ src/cr8tor/cli/__init__.py
@@ -3,6 +3,7 @@ from cr8tor.cli.create import app as create_command
 from cr8tor.cli.build import app as build_command
 from cr8tor.cli.validate import app as validate_command
 from cr8tor.cli.sign_off import app as sign_off_command
+from cr8tor.cli.disclosure import app as disclosure_command
 from cr8tor.cli.stage_transfer import app as stage_transfer_command
 from cr8tor.cli.publish import app as publish_command
 
@@ -15,5 +16,6 @@ app.add_typer(create_command)
 app.add_typer(build_command)
 app.add_typer(validate_command)
 app.add_typer(sign_off_command)
+app.add_typer(disclosure_command)
 app.add_typer(stage_transfer_command)
 app.add_typer(publish_command)
